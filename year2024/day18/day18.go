9e54edf84474e8cbf88e65d4e471da0e333c53cf
Kirill <44039371+vlone310@users.noreply.github.com>
2024-12-18 07:31:37 +0100

    chore: AOC 2024 day 18 (#40)
    Co-authored-by: Kirill Arekhouski <kirill.arekhouski@softswiss.com>

---
+++ year2024/day18/day18.go
@@ -0,0 +1,159 @@
+package year2024
+
+import (
+	"fmt"
+	"math"
+	"strings"
+
+	"github.com/vlone310/advent-of-code/helpers"
+	"github.com/vlone310/advent-of-code/pqueue"
+)
+
+type coords struct {
+	x, y int
+}
+
+type Node struct {
+	point    coords
+	g, h, f  int
+	previous *Node
+}
+
+var directions = [4][2]int{
+	{0, -1},
+	{0, 1},
+	{-1, 0},
+	{1, 0},
+}
+
+func ramRun(input string, gridSize, bytesSize int) int {
+	corrupted := getCorruptedCoordinates(input, bytesSize)
+	end := astar(gridSize, corrupted)
+
+	if end != nil {
+		return end.g
+	}
+
+	return 0
+}
+
+func ramRunP2(input string, gridZise, bytesSize int) string {
+	bytes := strings.Split(input, "\n")
+
+	for b := bytesSize; b < len(bytes); b++ {
+		corrupted := getCorruptedCoordinates(input, b)
+		end := astar(gridZise, corrupted)
+		if end == nil {
+			return bytes[b-1]
+		}
+	}
+
+	return ""
+}
+
+func getCorruptedCoordinates(input string, size int) map[coords]bool {
+	res := make(map[coords]bool)
+	rows := strings.Split(input, "\n")
+
+	for i := 0; i < size; i++ {
+		rawCoords := strings.Split(rows[i], ",")
+		if len(rawCoords) < 2 {
+			continue
+		}
+
+		res[coords{helpers.ParseInt(rawCoords[1]), helpers.ParseInt(rawCoords[0])}] = true
+	}
+
+	return res
+}
+
+func heuristic(a, b coords) int {
+	return int(math.Abs(float64(a.x-b.x)) + math.Abs(float64(a.y-b.y)))
+}
+
+func astar(gridSize int, corrupted map[coords]bool) *Node {
+	lessFunc := func(a, b *Node) bool {
+		return a.f < b.f
+	}
+
+	pq := pqueue.New(lessFunc)
+	start := coords{0, 0}
+	end := coords{gridSize, gridSize}
+
+	startNode := &Node{point: start, g: 0, h: heuristic(start, end), f: heuristic(start, end)}
+
+	pq.PushItem(startNode)
+
+	visited := make(map[coords]int)
+
+	for pq.Len() > 0 {
+		current := pq.PopItem()
+
+		if current.point == end {
+			return current
+		}
+
+		visited[current.point] = current.g
+
+		for _, dir := range directions {
+			point := coords{current.point.x + dir[0], current.point.y + dir[1]}
+			g := current.g + 1
+			h := heuristic(point, end)
+			f := g + h
+
+			if point.x < 0 || point.y < 0 || point.x > gridSize || point.y > gridSize {
+				continue
+			}
+
+			if corrupted[point] {
+				continue
+			}
+			if prevCost, exists := visited[point]; exists && prevCost <= g {
+				continue
+			}
+
+			pq.PushItem(&Node{
+				point:    point,
+				g:        g,
+				h:        h,
+				f:        f,
+				previous: current,
+			})
+
+		}
+	}
+
+	return nil
+}
+
+func traversePath(node *Node) map[coords]bool {
+	path := map[coords]bool{}
+	cur := node
+
+	for cur != nil {
+		path[cur.point] = true
+		cur = cur.previous
+	}
+
+	return path
+}
+
+func printGrid(gridSise int, corrupted map[coords]bool, path map[coords]bool) {
+	for i := 0; i <= gridSise; i++ {
+		row := ""
+		for j := 0; j <= gridSise; j++ {
+			next := "."
+			point := coords{i, j}
+			if corrupted[point] {
+				next = "#"
+			}
+
+			if path[point] {
+				next = "O"
+			}
+
+			row += next
+		}
+		fmt.Println(row)
+	}
+}
---
+++ year2024/day18/day18_test.go
@@ -0,0 +1,27 @@
+package year2024
+
+import (
+	"testing"
+
+	"github.com/vlone310/advent-of-code/helpers"
+)
+
+func TestRamRun(t *testing.T) {
+	input := helpers.ReadFile("./input.txt")
+	out := ramRun(input, 6, 12)
+	want := 22
+
+	if out != want {
+		t.Errorf("ramRun() = %v, want = %v", out, want)
+	}
+}
+
+func TestRamRunP2(t *testing.T) {
+	input := helpers.ReadFile("./input2.txt")
+	out := ramRunP2(input, 6, 12)
+	want := "6,1"
+
+	if out != want {
+		t.Errorf("ramRunP2() = %v, want = %v", out, want)
+	}
+}
